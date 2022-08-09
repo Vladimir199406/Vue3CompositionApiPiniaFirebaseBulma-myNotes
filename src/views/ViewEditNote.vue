732b05de7c408312438f8ccfc2b55b9777ef8e5f
vo <vovasso550@rambler.ru>
2022-08-10 00:25:43 +0300

    note edit proto layout

--- src/views/ViewEditNote.vue
+++ src/views/ViewEditNote.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="edit-note">
-    <h1>Edit Note with: {{ $route.params.id }}</h1>
+    <AddEditNote ref="addEditNoteRef" v-model="noteContent">
+      <template #buttons>
+        <button class="button is-link has-background-success">Save Note</button>
+      </template>
+    </AddEditNote>
   </div>
 </template>
+
+<script setup>
+/*
+ imports
+*/
+import { ref } from "vue";
+import AddEditNote from "@/components/notes/AddEditNote.vue";
+
+/*
+ data
+*/
+
+const noteContent = ref("");
+</script>
